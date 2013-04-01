27d5f56d072a21ed5e44f56394f1d676dab3d103
Joseph Pontani <jpontani@vt.edu>
2013-04-01 17:51:17 -0400

    last progress commit

--- lib/Drupal/Apachesolr/Facetapi/QueryType/DateRangeQueryType.php
+++ lib/Drupal/Apachesolr/Facetapi/QueryType/DateRangeQueryType.php
@@ -27,59 +27,47 @@ class Drupal_Apachesolr_Facetapi_QueryType_DateRangeQueryType extends FacetapiQu
     $field = $this->facet['field'];
     
     $settings = $this->adapter->getFacetSettings($this->facet, facetapi_realm_load('block'));
-    $ranges = (isset($settings->settings['ranges']) ? $settings->settings['ranges'] : array());
-
-    if (empty($ranges)) {
-      if (!empty($active)) {
-        // Check the first value since only one is allowed.
-        switch (key($active)) {
-          case 'past_hour':
-            list($start, $end) = $this->generateRange("-1|HOUR");
-            break;
-
-          case 'past_24_hours':
-            list($start, $end) = $this->generateRange("-1|DAY");
-            break;
-
-          case 'past_week':
-            list($start, $end) = $this->generateRange("-7|DAY", TRUE);
-            break;
-
-          case 'past_month':
-            list($start, $end) = $this->generateRange("-1|MONTH");
-            break;
-
-          case 'past_year':
-            list($start, $end) = $this->generateRange("-1|YEAR");
-            break;
-
-          default:
-            return;
-        }
+    $ranges = (isset($settings->settings['ranges']) ? $settings->settings['ranges'] : date_facets_default_ranges());
+    foreach (array_merge($ranges, date_facets_default_ranges()) as $range) {
+      $start_info = $range['date_range_start_op'] . $range['date_range_start_amount'] . '|' . $range['date_range_start_unit'];
+      if ($range['date_range_start_op'] == 'NOW') {
+        $start_info = NULL;
       }
+      $end_info = $range['date_range_end_op'] . $range['date_range_end_amount'] . '|' . $range['date_range_end_unit'];
+      if ($range['date_range_end_op'] == 'NOW') {
+        $end_info = NULL;
+      }
+      list($start, $end) = $this->generateRange($start_info, $end_info);
+      $query->addParam('facet.query', $this->facet['field'] . ":[$start TO $end]");
     }
-    else {
+
+    if (!empty($active)) {
       $range = $ranges[key($active)];
+      $start_info = $range['date_range_start_op'] . $range['date_range_start_amount'] . '|' . $range['date_range_start_unit'];
+      if ($range['date_range_start_op'] == 'NOW') {
+        $start_info = NULL;
+      }
+      $end_info = $range['date_range_end_op'] . $range['date_range_end_amount'] . '|' . $range['date_range_end_unit'];
+      if ($range['date_range_end_op'] == 'NOW') {
+        $end_info = NULL;
+      }
+      list($start, $end) = $this->generateRange($start_info, $end_info);
+      $query->addParam('fq', $this->facet['field'] . ":[$start TO $end]");
     }
-    $query->addParam('fq', $this->facet['field'] . ":[$start TO $end]");
   }
 
   /**
    * Return the divisor for the given unit.
    */
-  protected function unitDivisor($unit, $plural = FALSE) {
-    $s = '';
-    if ($plural) {
-      $s = 'S';
-    }
+  protected function unitDivisor($unit) {
     switch ($unit) {
       case "HOUR":
-        return "HOUR" . $s;
+        return "HOUR";
       case "DAY":
-        return "DAY" . $s;
+        return "DAY";
       case "MONTH":
       case "YEAR":
-        return "MONTH" . $s;
+        return "MONTH";
     }
   }
 
@@ -92,18 +80,18 @@ class Drupal_Apachesolr_Facetapi_QueryType_DateRangeQueryType extends FacetapiQu
     }
     else {
       list($lower_amount, $lower_unit) = explode('|', $lower);
-      $plural = ((int) abs($lower_amount) > 1);
-      $lower = "NOW/" . $this->unitDivisor($lower_unit, $plural) . $lower_amount;
+      $s = (((int) abs($lower_amount) > 1) ? 'S' : '');
+      $lower = "NOW/" . $this->unitDivisor($lower_unit) . $lower_amount . $lower_unit . $s;
     }
     if (empty($upper)) {
       $upper = "NOW/DAY+1DAY";
     }
     else {
       list($upper_amount, $upper_unit) = explode('|', $upper);
-      $plural = ((int) abs($upper_amount) > 1);
-      $upper = "NOW/" . $this->unitDivisor($upper_unit, $plural) . $upper_amount;
+      $s = (((int) abs($upper_amount) > 1) ? 'S' : '');
+      $upper = "NOW/" . $this->unitDivisor($upper_unit) . $upper_amount . $upper_unit . $s;
     }
-    return ($lower, $upper);
+    return array($lower, $upper);
   }
 
   /**
@@ -112,6 +100,32 @@ class Drupal_Apachesolr_Facetapi_QueryType_DateRangeQueryType extends FacetapiQu
    * Unlike normal facets, we provide a static list of options.
    */
   public function build() {
-    return date_facets_get_ranges();
+    $settings = $this->adapter->getFacetSettings($this->facet, facetapi_realm_load('block'));
+    $ranges = (isset($settings->settings['ranges']) ? $settings->settings['ranges'] : date_facets_default_ranges());
+    $build = date_facets_get_ranges($ranges);
+    if ($response = apachesolr_static_response_cache($this->adapter->getSearcher())) {
+      $facet_global_settings = $this->adapter->getFacet($this->facet)->getSettings();
+      $values = (array) $response->facet_counts->facet_queries;
+      // Add result counts from the facet queries added in execute().
+      foreach ($ranges as $range) {
+        $start_info = $range['date_range_start_op'] . $range['date_range_start_amount'] . '|' . $range['date_range_start_unit'];
+        if ($range['date_range_start_op'] == 'NOW') {
+          $start_info = NULL;
+        }
+        $end_info = $range['date_range_end_op'] . $range['date_range_end_amount'] . '|' . $range['date_range_end_unit'];
+        if ($range['date_range_end_op'] == 'NOW') {
+          $end_info = NULL;
+        }
+        list($start, $end) = $this->generateRange($start_info, $end_info);
+        $build[$range['machine_name']]['#count'] = $values[($this->facet['field'] . ":[$start TO $end]")];
+      }
+      // Unset options with fewer results than the minimum count setting.
+      foreach ($build as $name => $item) {
+        if ($item['#count'] < $facet_global_settings->settings['facet_mincount']) {
+          unset($build[$name]);
+        }
+      }
+    }
+    return $build;
   }
 }
