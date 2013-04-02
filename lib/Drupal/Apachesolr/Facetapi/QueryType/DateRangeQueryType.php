8772ded1763cdfe61ffd49c235ecc5b829a5c38c
Joseph Pontani <jpontani@vt.edu>
2013-04-02 17:53:53 -0400

    code cleanup

--- lib/Drupal/Apachesolr/Facetapi/QueryType/DateRangeQueryType.php
+++ lib/Drupal/Apachesolr/Facetapi/QueryType/DateRangeQueryType.php
@@ -29,28 +29,12 @@ class Drupal_Apachesolr_Facetapi_QueryType_DateRangeQueryType extends FacetapiQu
     $settings = $this->adapter->getFacetSettings($this->facet, facetapi_realm_load('block'));
     $ranges = (isset($settings->settings['ranges']) ? $settings->settings['ranges'] : date_facets_default_ranges());
     foreach (array_merge($ranges, date_facets_default_ranges()) as $range) {
-      $start_info = $range['date_range_start_op'] . $range['date_range_start_amount'] . '|' . $range['date_range_start_unit'];
-      if ($range['date_range_start_op'] == 'NOW') {
-        $start_info = NULL;
-      }
-      $end_info = $range['date_range_end_op'] . $range['date_range_end_amount'] . '|' . $range['date_range_end_unit'];
-      if ($range['date_range_end_op'] == 'NOW') {
-        $end_info = NULL;
-      }
-      list($start, $end) = $this->generateRange($start_info, $end_info);
+      list($start, $end) = $this->generateRange($range);
       $query->addParam('facet.query', $this->facet['field'] . ":[$start TO $end]");
     }
 
     if (!empty($active)) {
       $range = $ranges[key($active)];
-      $start_info = $range['date_range_start_op'] . $range['date_range_start_amount'] . '|' . $range['date_range_start_unit'];
-      if ($range['date_range_start_op'] == 'NOW') {
-        $start_info = NULL;
-      }
-      $end_info = $range['date_range_end_op'] . $range['date_range_end_amount'] . '|' . $range['date_range_end_unit'];
-      if ($range['date_range_end_op'] == 'NOW') {
-        $end_info = NULL;
-      }
       list($start, $end) = $this->generateRange($start_info, $end_info);
       $query->addParam('fq', $this->facet['field'] . ":[$start TO $end]");
     }
@@ -74,22 +58,20 @@ class Drupal_Apachesolr_Facetapi_QueryType_DateRangeQueryType extends FacetapiQu
   /**
    * Generate a Solr version of a lower and upper date range.
    */
-  protected function generateRange($lower = NULL, $upper = NULL) {
-    if (empty($lower)) {
+  protected function generateRange($range) {
+    if ($range['date_range_start_op'] == 'NOW') {
       $lower = "NOW/DAY";
     }
     else {
-      list($lower_amount, $lower_unit) = explode('|', $lower);
-      $s = (((int) abs($lower_amount) > 1) ? 'S' : '');
-      $lower = "NOW/" . $this->unitDivisor($lower_unit) . $lower_amount . $lower_unit . $s;
+      $s = (((int) abs($range['date_range_start_amount']) > 1) ? 'S' : '');
+      $lower = "NOW/" . $this->unitDivisor($range['date_range_start_unit']) . $range['date_range_start_amount'] . $range['date_range_start_unit'] . $s;
     }
-    if (empty($upper)) {
+    if ($range['date_range_end_op'] == 'NOW') {
       $upper = "NOW/DAY+1DAY";
     }
     else {
-      list($upper_amount, $upper_unit) = explode('|', $upper);
-      $s = (((int) abs($upper_amount) > 1) ? 'S' : '');
-      $upper = "NOW/" . $this->unitDivisor($upper_unit) . $upper_amount . $upper_unit . $s;
+      $s = (((int) abs($range['date_range_end_amount']) > 1) ? 'S' : '');
+      $upper = "NOW/" . $this->unitDivisor($range['date_range_end_unit']) . $range['date_range_end_amount'] . $range['date_range_end_unit'] . $s;
     }
     return array($lower, $upper);
   }
@@ -108,15 +90,7 @@ class Drupal_Apachesolr_Facetapi_QueryType_DateRangeQueryType extends FacetapiQu
       $values = (array) $response->facet_counts->facet_queries;
       // Add result counts from the facet queries added in execute().
       foreach ($ranges as $range) {
-        $start_info = $range['date_range_start_op'] . $range['date_range_start_amount'] . '|' . $range['date_range_start_unit'];
-        if ($range['date_range_start_op'] == 'NOW') {
-          $start_info = NULL;
-        }
-        $end_info = $range['date_range_end_op'] . $range['date_range_end_amount'] . '|' . $range['date_range_end_unit'];
-        if ($range['date_range_end_op'] == 'NOW') {
-          $end_info = NULL;
-        }
-        list($start, $end) = $this->generateRange($start_info, $end_info);
+        list($start, $end) = $this->generateRange($range);
         $build[$range['machine_name']]['#count'] = $values[($this->facet['field'] . ":[$start TO $end]")];
       }
       // Unset options with fewer results than the minimum count setting.
